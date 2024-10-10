e180a01b56ea5792d8f3b39a7432be3d1ff0ec01
LucasHSKKK <poqueni50@gmail.com>
2024-10-10 13:06:32 +0200

    new features

--- main_game/alien.invasion.py
+++ main_game/alien.invasion.py
@@ -6,9 +6,13 @@ import pygame
 class AlienInvasion:
     # manage game assets and resources
     def __init__(self):
+        # initialize the game, and create game resources
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
         pygame.display.set_caption("Alien Invasion")
+        # set the background color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         # starting the main loop
@@ -18,8 +22,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-        # make the most recently drawn screen visible.
-        pygame.display.flip()
+            # redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+            # make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == "__main__":
